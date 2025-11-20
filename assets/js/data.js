80a70bcc74d176422f55a7a923e2a1b44e9c9cb1
Jamexuu <jamesmercado2209@gmail.com>
2025-11-21 01:53:20 +0800

    refactor: improve data rendering for activies page

---
+++ assets/js/data.js
@@ -0,0 +1,14 @@
+var thirdYearProjects = [
+    {
+        title: "Lenovo Legion 5 Showcase",
+        description: "A showcase website for the Lenovo Legion 5 gaming laptop.",
+        previewLink: "https://jamexuu.github.io/Lenovo-Legion-5/",
+        githubLink: "https://github.com/Jamexuu/Lenovo-Legion-5"
+    },
+    {
+        title: "Jeju Island Showcase",
+        description: "A showcase website for the beautiful Jeju Island.",
+        previewLink: "https://jamexuu.github.io/Jeju-Island/",
+        githubLink: "https://github.com/Jamexuu/Jeju-Island"
+    }
+];
