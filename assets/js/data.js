443082aa843dd53b562fd15a0dc2fc9d7fd0b5f1
Jamexuu <jamesmercado2209@gmail.com>
2025-12-14 02:26:53 +0800

    add gallery project

--- assets/js/data.js
+++ assets/js/data.js
@@ -10,5 +10,11 @@ var thirdYearProjects = [
         description: "A showcase website for the beautiful Jeju Island.",
         previewLink: "https://jamexuu.github.io/Jeju-Island/",
         githubLink: "https://github.com/Jamexuu/Jeju-Island"
+    },
+    {
+        title: "Spotify Artists Gallery",
+        description: "A Gallery website for Spotify artists.",
+        previewLink: "https://jamexuu.github.io/Coding-with-Vibe/",
+        githubLink: "https://github.com/Jamexuu/Coding-with-Vibe"
     }
 ];
